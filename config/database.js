c4082101f329ecd1025b683a5ceddbee5cb811b7
brduarte <brunomacielduarte32@gmail.com>
2021-03-23 20:40:41 -0300

    salvando trabalho

--- config/database.js
+++ config/database.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 
 module.exports = {
-  username: process.env.DB_USER || 'postgres',
+  username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_DATABASE || 'crudcar',
-  host: process.env.DB_DATABASE || '127.0.0.1',
+  host: process.env.DB_HOST || '127.0.0.1',
   dialect: 'postgres',
 }
