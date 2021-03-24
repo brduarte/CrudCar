858224a161e74ba59d1e2d6522c636ee7c104c7d
brduarte <brunomacielduarte32@gmail.com>
2021-03-23 21:12:07 -0300

    Criando estrutura para veiculos

---
+++ database/migrations/20210324000857-create-vehicle.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Vehicles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      board: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      chassis: {
+        type: Sequelize.STRING
+      },
+      renavam: {
+        type: Sequelize.STRING
+      },
+      brand: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Vehicles');
+  }
+};
---
+++ src/models/vehicle.js
@@ -0,0 +1,29 @@
+'use strict';
+const {Model} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Vehicle extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+  };
+
+  Vehicle.init({
+    id: DataTypes.INTEGER,
+    board: DataTypes.STRING,
+    chassis: DataTypes.STRING,
+    renavam: DataTypes.STRING,
+    brand: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Vehicle',
+  });
+  return Vehicle;
+};
